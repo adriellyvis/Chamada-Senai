a9bd9523a51106114f8d7fdde99677ab611d3efd
KEVYN BRUNO DOS SANTOS LEAL <165089711+Kevynbr18@users.noreply.github.com>
2026-06-21 17:12:25 -0300

    Delete scriptBD Eyecount.sql

--- scriptBD Eyecount.sql
+++
@@ -1,9 +0,0 @@
--- Arquivo-ref: scriptBD Eyecount.sql
--- Este arquivo foi substituído por arquivos organizados em /db
--- Estrutura: db/schema.sql  (DDL idempotente e com charset)
--- Seeds:     db/seeds.sql   (inserts iniciais idempotentes)
--- Leia db/README.md para instruções de execução.
-
--- Observação: para executar localmente:
--- mysql -u <usuario> -p < db/schema.sql
--- mysql -u <usuario> -p < db/seeds.sql
